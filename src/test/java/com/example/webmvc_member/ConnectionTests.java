0fb084ed0a774171ab77bfe7922fe0cd0dfad6a1
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-08 17:16:04 +0900

    test: ConnectionTests에 testHikariCP 구현

--- src/test/java/com/example/webmvc_member/ConnectionTests.java
+++ src/test/java/com/example/webmvc_member/ConnectionTests.java
@@ -1,5 +1,7 @@
 package com.example.webmvc_member;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,32 @@ public class ConnectionTests {
             Assertions.assertNotNull(conn);
 
         } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testHikariCP() {
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3307/webdb");
+        config.setUsername("webuser");
+        config.setPassword("1234");
+
+        config.addDataSourceProperty("cachePrepStmts", true);
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource dataSource = new HikariDataSource(config);
+
+        try (
+                Connection conn = dataSource.getConnection()
+        ) {
+
+            log.info(conn);
+
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
