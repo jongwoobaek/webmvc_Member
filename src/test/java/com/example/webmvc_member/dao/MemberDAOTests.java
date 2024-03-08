13f3d4eeb83f23233fbb24854418163f956b19d7
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-08 17:47:02 +0900

    test: MemberDAOTests에 addMemberTest 구현

--- src/test/java/com/example/webmvc_member/dao/MemberDAOTests.java
+++ src/test/java/com/example/webmvc_member/dao/MemberDAOTests.java
@@ -1,4 +1,21 @@
 package com.example.webmvc_member.dao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MemberDAOTests {
+    MemberDAO memberDAO;
+    @Test
+    public void addMemberTest() throws Exception {
+        memberDAO = new MemberDAO();
+
+        String id = "testId";
+        String pw = "testPw";
+        String name = "testName";
+        String email = "test@test.test";
+
+        int result = memberDAO.addMember(id, pw, name, email);
+
+        Assertions.assertEquals(1, result);
+    }
 }
