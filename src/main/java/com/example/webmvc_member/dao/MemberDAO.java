eaae0c6bdf78bfd28d44b4dc1b0612503c05047b
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-09 11:55:28 +0900

    feat: MemberDao 클래스에 listMembers 메소드 구현

--- src/main/java/com/example/webmvc_member/dao/MemberDAO.java
+++ src/main/java/com/example/webmvc_member/dao/MemberDAO.java
@@ -1,12 +1,40 @@
 package com.example.webmvc_member.dao;
 
+import com.example.webmvc_member.domain.MemberVO;
 import com.example.webmvc_member.util.ConnectionUtil;
 import lombok.Cleanup;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberDAO {
+    public List<MemberVO> listMembers() throws Exception {
+        List<MemberVO> memberVOList = new ArrayList<>();
+        String sql = "SELECT * from tbl_member";
+
+        @Cleanup Connection conn = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement pstmt = conn.prepareStatement(sql);
+        @Cleanup ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            MemberVO memberVO = MemberVO.builder()
+                    .id("id")
+                    .pw("pw")
+                    .name("name")
+                    .email("email")
+                    .signupDate(LocalDate.parse("signupDate"))
+                    .build();
+
+            memberVOList.add(memberVO);
+        }
+
+        return memberVOList;
+    }
+
     public int addMember(String id, String pw, String name, String email) throws Exception {
         String sql = "INSERT INTO tbl_member (id, pw, name, email) values (?, ?, ?, ?)";
 
