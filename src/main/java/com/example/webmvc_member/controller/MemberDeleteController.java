e670ea8f34ee45758cf5ebe17ac32467ea9ba31b
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-09 17:40:53 +0900

    feat: MemberDeleteController 구현

---
+++ src/main/java/com/example/webmvc_member/controller/MemberDeleteController.java
@@ -0,0 +1,22 @@
+package com.example.webmvc_member.controller;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@Log4j2
+@WebServlet(name = "MemberDeleteController", urlPatterns = "/member/delMember.do")
+public class MemberDeleteController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
