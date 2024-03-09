c62889c31d215107c6b70bc3dd41b151c62154a8
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-09 16:38:28 +0900

    refactor: MemberListController 클래스 doGet 메소드에 memberVOList 가져오는 로직 구현

--- src/main/java/com/example/webmvc_member/controller/MemberListController.java
+++ src/main/java/com/example/webmvc_member/controller/MemberListController.java
@@ -1,15 +1,30 @@
 package com.example.webmvc_member.controller;
 
+import com.example.webmvc_member.domain.MemberVO;
+import com.example.webmvc_member.service.MemberService;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "MemberListController", urlPatterns = "/member/listMembers.do")
 public class MemberListController extends HttpServlet {
+    private MemberService memberService = MemberService.INSTANCE;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/member/list.jsp").forward(req, res);
+        List<MemberVO> memberVOList = null;
+
+        try {
+            memberVOList = memberService.listMembers();
+
+            req.setAttribute("memberList", memberVOList);
+            req.getRequestDispatcher("/WEB-INF/member/list.jsp").forward(req, res);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
