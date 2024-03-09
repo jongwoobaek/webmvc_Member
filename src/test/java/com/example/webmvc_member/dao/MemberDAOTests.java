426fb83ef6543cf6f2ad932f35416f3961f7153a
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-09 12:34:35 +0900

    refactor: addMemberTest 수정

--- src/test/java/com/example/webmvc_member/dao/MemberDAOTests.java
+++ src/test/java/com/example/webmvc_member/dao/MemberDAOTests.java
@@ -1,20 +1,36 @@
 package com.example.webmvc_member.dao;
 
+import com.example.webmvc_member.domain.MemberVO;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberDAOTests {
     MemberDAO memberDAO;
+    MemberVO memberVO;
+
+    @BeforeEach
+    public void ready() {
+        memberDAO = new MemberDAO();
+        memberVO = MemberVO.builder()
+                .id("test-id")
+                .pw("test - pw")
+                .name("test - name")
+                .email("test - email")
+                .build();
+    }
+
+    @AfterEach
+    public void afterEach() throws Exception {
+        memberDAO.deleteMember(memberVO);
+    }
+
     @Test
     public void addMemberTest() throws Exception {
         memberDAO = new MemberDAO();
 
-        String id = "testId";
-        String pw = "testPw";
-        String name = "testName";
-        String email = "test@test.test";
-
-        int result = memberDAO.addMember(id, pw, name, email);
+        int result = memberDAO.addMember(memberVO);
 
         Assertions.assertEquals(1, result);
     }
