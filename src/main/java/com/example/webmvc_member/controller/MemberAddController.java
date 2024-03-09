de11cb621818bdf1a20ef609e8a5da740db57597
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-09 17:06:46 +0900

    feat: MemberAddController 클래스 doPost 메소드 구현

--- src/main/java/com/example/webmvc_member/controller/MemberAddController.java
+++ src/main/java/com/example/webmvc_member/controller/MemberAddController.java
@@ -1,11 +1,16 @@
 package com.example.webmvc_member.controller;
 
+import com.example.webmvc_member.dto.MemberDTO;
+import com.example.webmvc_member.service.MemberService;
+import lombok.extern.log4j.Log4j2;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-@WebServlet(name = "addMemberController", urlPatterns = "/member/addMember.do")
+@Log4j2
+@WebServlet(name = "MemberAddController", urlPatterns = "/member/addMember.do")
 public class MemberAddController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -14,6 +19,26 @@ public class MemberAddController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        log.info("MemberAddController doPost called...!");
 
+        String id = req.getParameter("id");
+        String pw = req.getParameter("pw");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+
+        MemberDTO memberDTO = MemberDTO.builder()
+                .id(id)
+                .pw(pw)
+                .name(name)
+                .email(email)
+                .build();
+
+        try {
+            MemberService.INSTANCE.addMember(memberDTO);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        res.sendRedirect("/member/listMembers.do");
     }
 }
