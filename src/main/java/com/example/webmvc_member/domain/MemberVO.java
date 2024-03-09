f606440e57b5674061ec7b801a927475825d575f
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-09 15:14:19 +0900

    refactor: MemberVO 클래스에 어노테이션 추가

--- src/main/java/com/example/webmvc_member/domain/MemberVO.java
+++ src/main/java/com/example/webmvc_member/domain/MemberVO.java
@@ -1,12 +1,16 @@
 package com.example.webmvc_member.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberVO {
     private String id;
     private String pw;
